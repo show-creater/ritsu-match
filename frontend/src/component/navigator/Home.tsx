cf5539206e735e429a250f77bbde5d3dfdc1fd7a
natsumi <7bonheur723@gmail.com>
2024-06-18 19:31:31 +0900

    確認メール

--- frontend/src/component/navigator/Home.tsx
+++ frontend/src/component/navigator/Home.tsx
@@ -48,7 +48,7 @@ const Home = () => {
                     ),
                 }}>
                 </Tab.Screen>
-                <Tab.Screen  name='MyPage' component={MyPage}
+                <Tab.Screen  name='MyPage' component={SendEmail}
                     options={{
                         headerShown:false,
                         tabBarIcon: ({focused}) => (<Ionicons name="person-circle-outline" size={24} color='#30CB89' />
