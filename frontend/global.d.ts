d4b4c5994aeb882f37011b8879b1247534267d58
soshi1234 <148639191+soshi1234@users.noreply.github.com>
2024-06-29 20:40:16 +0900

    tailwindの導入

---
+++ frontend/global.d.ts
@@ -0,0 +1,15 @@
+// global.d.ts
+import 'react-native';
+
+declare module 'react-native' {
+  interface ViewProps {
+    className?: string;
+  }
+  interface TextProps {
+    className?: string;
+  }
+  interface TouchableOpacityProps {
+    className?: string;
+  }
+  // 他のコンポーネントも同様に定義できます
+}
