e00780d1a0f2dde158df0bba3786a33c5d7c456f
show-creator <git>
2024-07-09 02:02:56 +0900

    コンフリクト解消をしてchat_soshi_stash1とマージ正孔

--- frontend/src/component/context/HomeContext.js
+++ frontend/src/component/context/HomeContext.js
@@ -10,30 +10,19 @@ export const HomeProvider = ({ children }) => {
   const [loginUser, setLoginUser] = useState('');
   const [isTimeout, setIsTimeout] = useState(false);
   const [isTime, setIsTime] = useState(false);
-<<<<<<< HEAD
   const [myPageNow, setMyPageNow] = useState(true); //imagesettingとmypageの切り替え管理
   const [infor, setInfor] = useState({ name: '', heart: 0, faculty: '', image: '', age: 0, comment: '', heart_pushed: [], randomField: 0, userid: '' });
   const [userImage, setUserImage] = useState('');
   const [persondata, setPersondata] = useState([{ name: '', faculty: '', heart: '', image: '', age: 0, comment: '', heart_pushed: [], userid: '', randomField: '' }]);
   const [scrollcheck, setScrollcheck] = useState(false); //読み込みが完了してからスクロールが可能になるように設定
+  const [unreadMessagesJSON,setUnreadMessageJSON]=useState({})
+  const allChatMessages={}
 
   useLoadDoc({persondata, setPersondata, setScrollcheck, isLogin});
 
-  return (
-    <HomeContext.Provider value={{ isLogin, setIsLogin, loginUser, setLoginUser, isTimeout, setIsTimeout, isTime, setIsTime, myPageNow, setMyPageNow, infor, setInfor, userImage, setUserImage, persondata, setPersondata, scrollcheck, setScrollcheck }}>
-      {children}
-    </HomeContext.Provider>
-  );
-};
-=======
-  const [unreadMessagesJSON,setUnreadMessageJSON]=useState({})
-
-  const allChatMessages={}
-
     return (
-      <HomeContext.Provider value={{ isLogin, setIsLogin ,loginUser ,setLoginUser, talkPage, setTalkPage, isTimeout, setIsTimeout, isTime, setIsTime,unreadMessagesJSON,setUnreadMessageJSON,allChatMessages}}>
+      <HomeContext.Provider value={{ isLogin, setIsLogin, loginUser, setLoginUser, isTimeout, setIsTimeout, isTime, setIsTime, myPageNow, setMyPageNow, infor, setInfor, userImage, setUserImage, persondata, setPersondata, scrollcheck, setScrollcheck,unreadMessagesJSON,setUnreadMessageJSON,allChatMessages}}>
         { children }
       </HomeContext.Provider>
     );
   };
->>>>>>> 3c44b5353cef19baf92e3c6d343ce61db9f1df20
